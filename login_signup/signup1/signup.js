b6c92fc39a44787824d027d6d7a09dffe41c8c4e
Zouhir El baz <zel-baz@c2r9p2.1337.ma>
2024-01-18 23:24:49 +0100

    added signup page and some edits

---
+++ login_signup/signup1/signup.js
@@ -0,0 +1,16 @@
+let form = document.querySelector(".container form"),
+    passInput = document.getElementById("password"),
+    passIcon = document.querySelector(".container .passIcon");
+
+
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+});
+
+passIcon.addEventListener("click", ()=>{
+    if(passInput.type === "password"){
+        passInput.type = "text";
+    }else{
+        passInput.type = "password";
+    }
+});
